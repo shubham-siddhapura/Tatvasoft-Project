e228ed728dcade4e5128f425ef36ec1735f7f207
Shubham Siddhapura <shubhamsiddhapura55@gmail.com>
2022-01-26 00:45:43 +0530

    remake

---
+++ Helperland/Helperland/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Helperland.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helperland.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return PartialView();
+        }
+
+        public IActionResult About()
+        {
+            return PartialView();
+        }
+
+        public IActionResult Contact()
+        {
+            return PartialView();
+        }
+
+        public IActionResult Prices()
+        {
+            return PartialView();
+        }
+
+        public IActionResult Faq()
+        {
+            return PartialView();
+        }
+
+        public IActionResult Becomehelper()
+        {
+            return PartialView();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
