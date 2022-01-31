aab785f991e2ba3854a1a43396796343ad0bd874
Shubham Siddhapura <79081142+shubham-siddhapura@users.noreply.github.com>
2022-02-01 00:24:02 +0530

    update startup.cs file

--- Helperland/Helperland/Startup.cs
+++ Helperland/Helperland/Startup.cs
@@ -1,3 +1,4 @@
+using Helperland.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,7 @@ namespace Helperland
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<HelperlandContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
